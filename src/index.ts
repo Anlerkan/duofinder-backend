89c0bcc8dcc6f1068fbffd7001139d9ff03f10a0
Anılerkan <43711824+Anlerkan@users.noreply.github.com>
2021-11-13 23:52:02 +0300

    build: read port from env variables

--- src/index.ts
+++ src/index.ts
@@ -22,5 +22,5 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
-  .then(() => app.listen(3000, () => console.log('Backend is running')))
+  .then(() => app.listen(process.env.PORT || 5000, () => console.log('Backend is running')))
   .catch((err) => console.log(err));
